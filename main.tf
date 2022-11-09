b037a522f4f29ed5e23fcae069b35523eaae0ed5
Gizem Pesen <pesengizem@gmail.com>
2022-11-09 23:15:33 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -83,7 +83,7 @@ resource "null_resource" "remote-exec" {
       type ="ssh"
       agent = false
       user = "ubuntu"
-      private_key = "${file("/home/ubuntu/blc-terraform-docker/blc-cloud.pem")}"
+      private_key = "${file("${path.module}//blc-cloud.pem")}"
       host = "${openstack_networking_floatingip_v2.admin.address}"
     }
     inline = [      
